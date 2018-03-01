032324d8b3f3a72550afe43aca3853afaba5e0b5
Caleb <cwill.i.am.tomlin@gmail.com>
2018-02-28 19:56:31 -0600

    fixed depth on tar.sh

--- tarmaker.sh
+++ tarmaker.sh
@@ -6,5 +6,5 @@ echo "usage $(basename $0) [directory]"
 exit 1
 fi
 
-find $1 -type f -name "*.cpp" -o -name "*.h" -o -name "[Mm]akefile" 2> /dev/null | tar -cf cbp$(date +%F)\
-.tar --files-from -
+find $1 -maxdepth 1 -type f -name "*.cpp" -o -name "*.h" \
+ -o -name "[Mm]akefile" 2> /dev/null | tar -cf cbp$(date +%F).tar --files-from -
